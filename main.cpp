1fcec876ad7b3465ffec85bd6ea65da036d842c6
Makenzie De Armas <dearm102@mail.chapman.edu>
2019-02-07 19:30:32 +0000

    Created main file.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+  Makenzie De Armas
+  ID: 2278709
+  dearm102@mail.chapman.edu
+  CPSC 350-01
+  Assignment 1: C++ Review
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  
+}
