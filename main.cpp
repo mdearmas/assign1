cad719f05613b5d64d9c4202709982684919b026
Makenzie De Armas <dearm102@mail.chapman.edu>
2019-02-10 23:30:52 +0000

    Added file input and output. First attempt.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,40 @@
 */
 
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-  
+  ifstream inputFile;
+  string filepath;
+  ofstream outputFile("mdearmas.txt");
+
+  int lineCount;
+
+  if( outputFile.is_open() )
+  {
+    outputFile << "Makenzie De Armas" << endl;
+    outputFile << "ID: 2278709" << endl;
+    outputFile << "dearm102@mail.chapman.edu" << endl;
+    outputFile << "CPSC 350-01, Assignment 1" << endl;
+    outputFile << endl;
+  }
+
+  cout << "Please enter your file name: ";
+  cin >> filepath;
+
+  inputFile.open(filepath);
+  if ( filepath.is_open() )
+  {
+    while ( getline(filepath, line) )
+    {
+      lineCount++;
+      outputFile << line << endl;
+    }
+    outputFile << lineCount << endl;
+  }
+
+  inputFile.close();
+  outputFile.close();
 }
